f15444d020df1541c7e87e96fb689bdfd4174602
andrewnft <154650200+andrewnft@users.noreply.github.com>
2023-12-23 19:57:35 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("E5PhYhyZma92Xi4cD7YnvzJRXiYMUptZ4rvN1aJMWKsq");
 
 #[program]
 pub mod nft_lend_borrow {
